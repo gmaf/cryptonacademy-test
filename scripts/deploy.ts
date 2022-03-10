ea60075065fcfe4d7bd02cf1af0ae1f5de477759
Georgii Afanasev <gmafsgu@gmail.com>
2022-03-10 19:16:28 +0300

    Change deploy script to include more information

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,15 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-  const Donations = await ethers.getContractFactory('Donations');
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Donations = await ethers.getContractFactory("Donations");
   const donations = await Donations.deploy();
-
   await donations.deployed();
-
   console.log('Donations deployed to:', donations.address);
 }
 
